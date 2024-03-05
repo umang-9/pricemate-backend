7631fe8c8e6ce4dab4676fc1959cfb448f6fe50c
shubham-00 <srpatel980@gmail.com>
2024-03-05 13:45:03 -0500

    added password strength validation in password reset feature.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -65,3 +66,7 @@ class LoginSerializer(serializers.Serializer):
 class PasswordResetSerializer(serializers.Serializer):
     old_password = serializers.CharField(required=True)
     new_password = serializers.CharField(required=True)
+
+    def validate_new_password(self, value):
+        validate_password(value)
+        return value
--- docs/auth/reset-password.md
+++ docs/auth/reset-password.md
@@ -95,3 +95,37 @@ Response
 
 `Status code: 401 UNAUTHORIZED`
 Consider deleting the token from the frontend!
+
+# Example 4
+
+Invalid data (weak password)
+
+```
+{
+    'old_password': '****',
+    'new_password': '****invalid****',
+}
+```
+
+Valid Authorization
+
+```
+{
+    'Authorization': 'Token ****',
+}
+```
+
+Response
+
+```
+{
+{
+    "new_password": [
+        "This password is too short. It must contain at least 8 characters.",
+        "This password is too common.",
+        "This password is entirely numeric."
+    ]
+}}
+```
+
+`Status code: 400 BAD REQUEST`
