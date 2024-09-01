5be3e3c3e770ef3209191ba0b40848efe230a78f
Umang Patel <patelumang858@gmail.com>
2024-09-01 14:24:11 -0400

    Add Host URL

--- website/settings.py
+++ website/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-a1cpps($jf=qsw4a=5dz$yq$ex53_6m8fg9!)s+(_ins8kkv=!"
 
 DEBUG = True
-ALLOWED_HOSTS = ["vibesok.com", "b.vibesok.com"]
+ALLOWED_HOSTS = ["pricemate-backend.onrender.com"]
 
 
 if DEBUG:
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -102,10 +103,25 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
-MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media"
+# STATIC_URL = "static/"
+# STATIC_ROOT = BASE_DIR / "static"
+# MEDIA_URL = "media/"
+# MEDIA_ROOT = BASE_DIR / "media"
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+# This setting informs Django of the URI path from which your static files will be served to users
+# Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
+STATIC_URL = '/static/'
+
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
@@ -136,7 +152,7 @@ DJANGO_REST_MULTITOKENAUTH_RESET_TOKEN_EXPIRY_TIME = 1
 DJANGO_REST_PASSWORDRESET_NO_INFORMATION_LEAKAGE = True
 
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "https://vibesok.com", "http://127.0.0.1:8000", "http://localhost:8000"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "https://pricemate-backend.onrender.com", "http://127.0.0.1:8000", "http://localhost:8000"]
 
 CSRF_COOKIE_SECURE = False
 SESSION_COOKIE_SECURE = False
